9a61e7f057d8f49f693610689e7c0ff2645a03b0
mostafaelspagh0 <mostafaelspagh0@gmail.com>
2022-05-17 14:08:55 +0200

    moving

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
+/* eslint-disable react-native/no-inline-styles */
 // In App.js in a new project
 
 import * as React from 'react';
-import {View, Text, ScrollView, Button, TextInput, Alert} from 'react-native';
+import {ScrollView, Button, TextInput, Alert, View, Text} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {addCategory} from './src/services/firestore';
+import {Picker} from '@react-native-picker/picker';
 
 function AdminScreen({navigation}: any) {
   return (
@@ -15,11 +17,87 @@ function AdminScreen({navigation}: any) {
           navigation.navigate('AddCategory');
         }}
       />
+      <Button
+        title="add product"
+        onPress={() => {
+          navigation.navigate('AddProduct');
+        }}
+      />
     </ScrollView>
   );
 }
 
-function AddCategory({navigation}: any) {
+const AddProduct = () => {
+  const [name, setName] = React.useState('');
+  const [selectedBrand, setSelectedBrand] = React.useState();
+  const [brands, setBrands] = React.useState(['samsung', 'apple']);
+  // const [images, setImages] = React.useState([]);
+  return (
+    <ScrollView>
+      <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          padding: 5,
+        }}>
+        <Text>Name</Text>
+        <TextInput
+          style={{
+            borderWidth: 1,
+            flex: 1,
+            margin: 10,
+          }}
+          placeholder="name"
+          value={name}
+          onChangeText={setName}
+        />
+      </View>
+      <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          padding: 5,
+        }}>
+        <Text>brand</Text>
+        <Picker
+          style={{
+            borderWidth: 1,
+            flex: 1,
+          }}
+          selectedValue={selectedBrand}
+          onValueChange={itemValue => setSelectedBrand(itemValue)}>
+          {brands.map(item => {
+            return <Picker.Item value={item} label={item} />;
+          })}
+        </Picker>
+      </View>
+      <View
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          padding: 5,
+        }}>
+        <Text>brand</Text>
+        <Picker
+          style={{
+            borderWidth: 1,
+            flex: 1,
+          }}
+          selectedValue={selectedBrand}
+          onValueChange={itemValue => setSelectedBrand(itemValue)}>
+          {brands.map(item => {
+            return <Picker.Item value={item} label={item} />;
+          })}
+        </Picker>
+      </View>
+
+      <ScrollView horizontal></ScrollView>
+      <Button title="add" onPress={() => {}} />
+    </ScrollView>
+  );
+};
+
+function AddCategory() {
   const [category, setCategory] = React.useState('');
   const [loading, setLoading] = React.useState(false);
   return (
@@ -50,6 +128,7 @@ function App() {
       <Stack.Navigator>
         <Stack.Screen name="Admin" component={AdminScreen} />
         <Stack.Screen name="AddCategory" component={AddCategory} />
+        <Stack.Screen name="AddProduct" component={AddProduct} />
       </Stack.Navigator>
     </NavigationContainer>
   );
